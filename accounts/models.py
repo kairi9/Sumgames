0198a729b6c33c5ece733d8eaab57588fd57415b
tky2002058 <tky2002058@stu.o-hara.ac.jp>
2021-10-19 10:25:42 +0900

    sou_10_19

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from typing import AbstractSet
 from django.db import models
 
 # Create your models here.
+
+class CustoUser(AbstractUser):
+    """拡張ユーザモデル"""
+
+    class Meta:
+        verbose_name_plural='CustomUser'
