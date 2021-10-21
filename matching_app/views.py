4751072face7ab4fb2cf84dbf318f370c8dd6ad3
garam16 <raterate0224@gmail.com>
2021-10-21 11:03:49 +0900

    munakata_10_21

--- matching_app/views.py
+++ matching_app/views.py
@@ -5,3 +5,39 @@ from django.views.generic import View
 
 class TopPageView(View):
     pass
+
+class LoginView(View):
+    pass
+
+class SignupView(View):
+    pass
+
+class PasswordView(View):
+    pass
+
+class VerificationView(View):
+    pass
+
+class HostformView(View):
+    pass
+
+class GameView(View):
+    pass
+
+class GuestconfirmationView(View):
+    pass
+
+class TalkroomView(View):
+    pass
+
+class NftindexView(View):
+    pass
+
+class NftdoneView(View):
+    pass
+
+class ProfileView(View):
+    pass
+
+class Profile_editView(View):
+    pass
