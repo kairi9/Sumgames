14d114659eee7c005552e4de7084b437ffc5d64e
tky2002058 <tky2002058@stu.o-hara.ac.jp>
2021-10-21 10:50:31 +0900

    sou_10_21

--- matching_app/urls.py
+++ matching_app/urls.py
@@ -5,5 +5,17 @@ from . import views
 
 app_name = 'matching'
 urlpatterns = [
-    path('', views.TopPageView.as_view(), name="top")
+    path('', views.TopPageView.as_view(), name="top"),
+    path('',views.LoginView.as_view,name="login"),
+    path('',views.SignupView.as_view,name="signup"),
+    path('',views.Passwordview.as_view,name="password"),
+    path('',views.VerificationView.as_view,name="verification"),
+    path('',views.HostformView.as_view,name="hostform"),
+    path('',views.Game.View.as_view,name="game"),
+    path('',views.GuestconfirmationView.as_view,name="guestconfirmation"),
+    path('',views.TalkroomView.as_view,name="talkroom"),
+    path('',views.IndexView.as_view,name="index"),
+    path('',views.DoneView.as_view,name="done"),
+    path('',views.ProfileView.as_view,name="profile"),
+    path('',views.Profile_editView.as_view,name="profile_edit"),
 ]
